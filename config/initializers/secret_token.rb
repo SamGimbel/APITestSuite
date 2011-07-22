387bfea07db9790d97f03e469aeb9bfd152b641e
Sam Gimbel <me@samgimbel.com>
2011-07-22 14:14:54 -0400

    rails creation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+APITestSuite::Application.config.secret_token = '7790e233d7032967687a368eba0e0e4310695a4df1ef4c8d7404edd6e8e5cc2f479f4f04f20230aba011518ef510b91a5ba8012b747ab33af58736db0ca48d56'
